fad9a9017b7ab8205087e0dae815ac0c25472a2a
Christian Oder <me@myself5.de>
2025-06-04 01:36:12 +0200

    Farbverbrauch: Properly theme help arrow

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "de.myself5.farbverbrauch"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5
-        versionName = "2.0"
+        versionCode = 6
+        versionName = "2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
